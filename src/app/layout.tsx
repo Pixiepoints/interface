ae13302101fbd9ce4d71b40f54c5ea15239c4afd
tim <tim@eforest.finance>
2024-04-02 20:51:25 +0800

    feature: add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,16 +31,24 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-87ZFKS0JQH" />
         <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-87ZFKS0JQH');
-        `}
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5WSTGQ7F');`}
         </Script>
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: (
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-5WSTGQ7F"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"></iframe>
+            ),
+          }}></noscript>
         <Provider>
           <Layout>{children}</Layout>
         </Provider>
