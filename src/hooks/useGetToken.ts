98891ec7736144644bf10f219885175abba5416f
navelle-l <navelle@pixiepoints.io>
2024-04-03 14:51:20 +0800

    feat: 🎸 update address data when switching accounts

--- src/hooks/useGetToken.ts
+++ src/hooks/useGetToken.ts
@@ -12,7 +12,14 @@ import { setHasToken } from 'redux/reducer/info';
 const AElf = require('aelf-sdk');
 
 export const useGetToken = () => {
-  const { loginState, wallet, getSignature, walletType, version } = useWebLogin();
+  const {
+    loginState,
+    wallet,
+    wallet: { address },
+    getSignature,
+    walletType,
+    version,
+  } = useWebLogin();
 
   const { getSignatureAndPublicKey } = useDiscoverProvider();
 
@@ -36,12 +43,12 @@ export const useGetToken = () => {
     if (loginState !== WebLoginState.logined) return false;
     const accountInfo = JSON.parse(localStorage.getItem(storages.accountInfo) || '{}');
 
-    if (accountInfo?.token && Date.now() < accountInfo?.expirationTime && accountInfo.account === wallet.address) {
+    if (accountInfo?.token && Date.now() < accountInfo?.expirationTime && accountInfo.account === address) {
       return true;
     } else {
       return false;
     }
-  }, [loginState, wallet.address]);
+  }, [loginState, address]);
 
   const getToken = useCallback(async () => {
     if (loginState !== WebLoginState.logined) return;
@@ -99,7 +106,7 @@ export const useGetToken = () => {
       publickey: publicKey,
       address: wallet.address,
     } as ITokenParams);
-  }, [loginState, getSignature, wallet]);
+  }, [loginState, wallet.address, wallet.publicKey, walletType, runAsync, getSignatureAndPublicKey, getSignature]);
 
   return { getToken, checkTokenValid };
 };
