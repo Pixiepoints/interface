10a6438e2ad55983df51ebc74fd2947bd9322906
omtre <shunert@pixiepoints.io>
2024-05-13 10:46:01 +0800

    feat: text lint

--- src/pageComponents/home/index.tsx
+++ src/pageComponents/home/index.tsx
@@ -7,6 +7,7 @@ export default function HomePage() {
   const { dAppList, loading, onSearch } = useHomeLogicService();
   return (
     <div className="flex flex-col max-w-[1360px] mx-auto px-4">
+      <span>test lint</span>
       <div className="py-8 my-6 md:py-[60px] md:mt-12 md:mb-20">
         <h1 className="text-center font-semibold text-neutralTitle text-6xl mb-12">
           {'Become dApp Advocate and Earn Points'}
